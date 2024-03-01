1255d7f38f83d5412f727fe60f8bfc02c909288d
ledinhhuy <huy@ioz.jp>
2024-03-01 23:01:30 +0700

    change some function's name

--- Slot_14/assignment_array.c
+++ Slot_14/assignment_array.c
@@ -3,64 +3,64 @@
 
 const int MAX = 100;
 
-int check_even_number (int n) {
+int isEvenNumber (int n) {
 	if (abs(n) % 2 == 0) return 1;
 	else return 0;
 }
 
-int check_prime_number (int n) {
+int isPrimeNumber (int n) {
 	int i, count = 0;
-	
+
 	for (i = 1; i <= n; i++) {
 		if (n % i == 0) {
 			count++;
 		}
 	}
-	
+
 	if (count == 2) return 1;
 	else return 0;
 }
 
-int print_prime_number_by_index (int index) {
+int pickPrimeNumberByIndex (int index) {
 	int i, number, count = 0;
-	
+
 	for (i = 2; count <= index; i++) {
-		if (check_prime_number(i) == 1) {
+		if (isPrimeNumber(i) == 1) {
 			count = count + 1;
 			number = i;
 		}
 	}
-	
+
 	return number;
 }
 
 int sumArrToNumber (int a[], int n) {
 	int i, result = 0, j = n - 1;
-	
+
 	for (i = 0; i < n; i++) {
 		result = result * 10 + a[i];
 		j--;
 	}
-	
+
 	return result;
 }
 
-int symmetricalNumber (int n) {
+int isSymmetricalNumber (int n) {
 	int result = 0, d;
-	
+
 	while (n != 0) {
 		d = n % 10;
 		result = result * 10 + d;
 		n = n / 10;
 	}
-	
+
 	return result;
 }
 
-void input_array (int a[], int *n) {
+void intputArr (int a[], int *n) {
 	printf("Nhap n: ");
 	scanf("%d", n);
-	
+
 	int i;
 	for (i = 0; i < *n; i++) {
 		printf("a[%d]: ", i);
@@ -68,17 +68,17 @@ void input_array (int a[], int *n) {
 	}
 }
 
-void find_max_even_array (int a[], int n) {
+void findMaxEvenArr (int a[], int n) {
 	int i, success = 0;
 	int min = a[0];
-	
+
 	for (i = 1; i < n; i++) {
 		if (a[i] < min) min = a[i];
 	}
-	
+
 	int max = min;
 	for (i = 0; i < n; i++) {
-		if (check_even_number(a[i]) == 1 && a[i] > max) {
+		if (isEvenNumber(a[i]) == 1 && a[i] > max) {
 			max = a[i];
 			success = 1;
 		}
@@ -88,59 +88,59 @@ void find_max_even_array (int a[], int n) {
 	else printf("Khong co so chan nao trong mang!!! \n");
 }
 
-void find_sum_prime_array (int a[], int n) {
+void findSumOfAveragePrimeArr (int a[], int n) {
 	int i, sum = 0, count = 0;
-	
+
 	for (i = 0; i < n; i++) {
-		if (check_prime_number(a[i]) == 1) {
+		if (isPrimeNumber(a[i]) == 1) {
 			sum += a[i];
 			count++;
 		}
 	}
-	
+
 	printf("Bai 2: \n");
-	if (count > 0) 
+	if (count > 0)
 		printf("Trung binh cong cac so nguyen to trong mang: %.2f \n", (float) sum / count);
-	else 
+	else
 		printf("Trung binh cong cac so nguyen to trong mang: 0 \n");
 }
 
-void create_fibonacci (int a[], int n) {
+void createFibonacci (int a[], int n) {
 	int i;
 	a[0] = 1;
 	a[1] = 1;
-	
+
 	for (i = 2; i < n; i++) {
 		a[i] = a[i - 1] + a[i - 2];
 	}
-	
+
 	printf("Bai 3:\n%d so fibonacci: ", n);
 	for (i = 0; i < n; i++) {
 		printf("%d ", a[i]);
 	}
-	printf("\n"); 
+	printf("\n");
 }
 
-void find_ave_min_max (int a[], int n) {
+void findSumOfAverageMaxMin (int a[], int n) {
 	int i, min = a[0], max = a[0];
-	
+
 	for (i = 1; i < n; i++) {
 		if (a[i] < min) min = a[i];
-	} 
-	
+	}
+
 	for (i = 1; i < n; i++) {
 		if (a[i] > max) max = a[i];
 	}
-	
+
 	printf("Bai 4:\nTrung binh cong 2 so max va min trong mang: %.2f \n", (max + min) / 2.0);
 }
 
-void find_index_x (int a[], int n, int *x) {
+void findIndexByValue (int a[], int n, int *x) {
 	int i, index;
-	
+
 	printf("Bai 5:\nNhap so nguyen x: ");
 	scanf("%d", x);
-	
+
 	printf("Vi tri cua so nguyen x trong mang: ");
 	for (i = 0; i < n; i++) {
 		if (a[i] == *x) {
@@ -151,86 +151,90 @@ void find_index_x (int a[], int n, int *x) {
 	printf("\n");
 }
 
-void swap_max_to_min (int a[], int n) {
+void swapMaxToMin (int a[], int n) {
 	int i, min = a[0], max = a[0];
-	
+
 	for (i = 1; i < n; i++) {
 		if (a[i] < min) min = a[i];
 		if (a[i] > max) max = a[i];
 	}
-	
+
 	for (i = 0; i < n; i++) {
 		if (a[i] == max) a[i] = min;
 	}
-	
+
 	printf("Mang sau khi swap: ");
 	for (i = 0; i < n; i++) {
 		printf("%d ", a[i]);
 	}
-	
+
 	printf("\n");
 }
 
-void swap_prime_index (int a[], int n) {
+void swapPrimeNumberByIndex (int a[], int n) {
 	int i, min = a[0], index = 1;
-	
+
 	for (i = 1; i < n; i++) {
 		if (a[i] < min) min = a[i];
 	}
-	
+
 	for (i = 0; i < n; i++) {
 		if (a[i] == min) {
-			a[i] = print_prime_number_by_index(i);
+			a[i] = pickPrimeNumberByIndex(i);
 		}
 	}
-	
+
 	printf("Mang sau khi thay doi gia tri: ");
 	for (i = 0; i < n; i++) {
 		printf("%d ", a[i]);
 	}
 	printf("\n");
-} 
+}
 
-void isSymmetrical (int a[], int n) {
+void isSymmetricalArr (int a[], int n) {
 	int number = sumArrToNumber(a, n);
-	int isSymmetric = symmetricalNumber(number);
-	
+	int isSymmetric = isSymmetricalNumber(number);
+
 	printf("Bai 8: \n");
-	if (isSymmetric == number) printf("True \n");
-	else printf("False \n");
+	int i;
+	for (i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
+	if (isSymmetric == number) printf("This is symmetrical array!!! \n");
+	else printf("This is not symmetrical array!!! \n");
 }
 
 int main () {
-	int arr[MAX], a[MAX], a_swap[MAX], a_swap_prime[MAX], n, x;
-	input_array(arr, &n);
-	
+	int arr[MAX], a[MAX], aSwap[MAX], aSwapPrime[MAX], n, x;
+	intputArr(arr, &n);
+
 	//Bai 1:
-	find_max_even_array(arr, n);
-	
+	findMaxEvenArr(arr, n);
+
 	//Bai 2:
-	find_sum_prime_array(arr, n);
-	
+	findSumOfAveragePrimeArr(arr, n);
+
 	//Bai 3:
-	create_fibonacci(a, n);
-	
+	createFibonacci(a, n);
+
 	//Bai 4:
-	find_ave_min_max(arr, n);
-	
+	findSumOfAverageMaxMin(arr, n);
+
 	//Bai 5:
-	find_index_x(arr, n, &x);
+	findIndexByValue(arr, n, &x);
 
 	//Bai 6:
 	printf("Bai 6: \n");
-	input_array(a_swap, &n);
-	swap_max_to_min(a_swap, n);
+	intputArr(aSwap, &n);
+	swapMaxToMin(aSwap, n);
 
 	//Bai 7:
 	printf("Bai 7: \n");
-	input_array(a_swap_prime, &n);
-	swap_prime_index(a_swap_prime, n);
+	intputArr(aSwapPrime, &n);
+	swapPrimeNumberByIndex(aSwapPrime, n);
 
-	//Bai 8: 
-	isSymmetrical(arr, n);
+	//Bai 8:
+	isSymmetricalArr(arr, n);
 
 	return 0;
 }
